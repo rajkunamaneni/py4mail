65f8c25c037613d4af68502674e87295496115cb
Isuph Shrestha <shresthaisuph2@gmail.com>
2023-05-30 05:08:40 -0700

    some front-end

--- apps/py4mail/controllers.py
+++ apps/py4mail/controllers.py
@@ -10,21 +10,63 @@ from .models import get_username
 
 # url_signer = URLSigner(session)
 
+def get_elapsed_time(created_on):
+    now = datetime.datetime.utcnow()
+
+    # Calculate the time difference in seconds
+    elapsed = now - created_on
+    elapsed_seconds = elapsed.total_seconds()
+
+    # Format the elapsed time based on different conditions
+    if elapsed_seconds < 60:
+        elapsed_time = "Just now"
+    elif elapsed_seconds < 3600:
+        minutes = int(elapsed_seconds / 60)
+        elapsed_time = f"{minutes} min{'s' if minutes > 1 else ''} ago"
+    elif created_on.year < now.year:
+        elapsed_time = created_on.strftime("%m/%d/%Y")
+    elif created_on.date() == now.date():
+        elapsed_time = created_on.strftime("%I:%M %p")
+    else:
+        elapsed_time = created_on.strftime("%B %d")
+
+    return elapsed_time
+
 @action('index')
 @action.uses('index.html', db, auth.user)
 def index():
     return dict(get_emails_url = URL('get_emails'))
 
+# @action("get_emails")
+# @action.uses(db, auth.user)
+# def get_emails():
+#     all_emails = db(db.emails.receiver_id == auth.user_id).select().as_list()
+#     star_emails = db((db.emails.receiver_id == auth.user_id) & (db.emails.isStarred == True)).select().as_list()
+#     trash_emails = db((db.emails.receiver_id == auth.user_id) & (db.emails.isTrash == True)).select().as_list()
+#     return dict(emails=emails,
+#                 star_emails=star_emails,
+#                 trash_emails=trash_emails,
+#     )
 @action("get_emails")
 @action.uses(db, auth.user)
 def get_emails():
-    all_emails = db(db.emails.receiver_id == auth.user_id).select().as_list()
-    star_emails = db((db.emails.receiver_id == auth.user_id) & (db.emails.isStarred == True)).select().as_list()
-    trash_emails = db((db.emails.receiver_id == auth.user_id) & (db.emails.isTrash == True)).select().as_list()
-    return dict(emails=emails,
-                star_emails=star_emails,
-                trash_emails=trash_emails,
-    )
+    emails = db(db.emails.receiver_id == auth.user_id).select().as_list()
+
+    # Retrieve sender names from auth_user table
+    sender_ids = [email['sender_id'] for email in emails]
+    sender_info = db(db.auth_user.id.belongs(sender_ids)).select()
+    sender_names = {sender.id: f"{sender.first_name} {sender.last_name}" for sender in sender_info}
+
+    # Retrieve receiver name from auth_user table
+    receiver_info = db.auth_user[auth.user_id]
+    receiver_name = f"{receiver_info.first_name} {receiver_info.last_name}"
+
+    # Update emails list with sender and receiver names, and elapsed time
+    for email in emails:
+        email['sender_name'] = sender_names.get(email['sender_id'])
+        email['receiver_name'] = receiver_name
+        email['elapsed_time'] = get_elapsed_time(email['sent_at'])
+    return dict(emails=emails)
 
 @action("move_to_trash")
 @action.uses(db, auth.user)
